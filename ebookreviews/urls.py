f120af6f01168039a08fc3d53efdd00f40a4ad41
muhammadanang <muhanangmahrub@gmail.com>
2022-12-23 08:44:01 +0700

    setting models and storing images

--- ebookreviews/urls.py
+++ ebookreviews/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ebook import views as ebookViews
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,3 +25,5 @@ urlpatterns = [
     path("about/", ebookViews.about, name="about"),
     path("signup/", ebookViews.signup, name="signup"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
