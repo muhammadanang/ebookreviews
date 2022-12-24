6ec8affd397975e46075422882a66d4a1caa4d3c
muhammadanang <muhanangmahrub@gmail.com>
2022-12-24 14:13:34 +0700

    configuring models in the news app

--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
 def news(request):
-    return render(request, "news.html")
+    newss = News.objects.all().order_by("-date")
+    return render(request, "news.html", {"newss": newss})
