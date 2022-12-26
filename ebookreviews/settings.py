20f583293853ac4f6086baa2c866aa33466bdb4f
muhammadanang <muhanangmahrub@gmail.com>
2022-12-26 10:54:27 +0700

    creating a base template

--- ebookreviews/settings.py
+++ ebookreviews/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "ebookreviews.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "ebookreviews/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
