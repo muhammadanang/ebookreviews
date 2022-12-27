7980242140769c52aba3337aadf3b51249864953
muhammadanang <muhanangmahrub@gmail.com>
2022-12-27 10:50:47 +0700

    set limited access for not-logged-in users

--- ebook/views.py
+++ ebook/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 from .models import Ebook, Review
 from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -27,14 +28,21 @@ def signup(request):
 
 def detail(request, ebook_id):
     ebook = get_object_or_404(Ebook, pk=ebook_id)
-    return render(request, "detail.html", {"ebook": ebook})
+    reviews = Review.objects.filter(ebook=ebook)
+    return render(request, "detail.html", {"ebook": ebook, "reviews": reviews})
 
 
+@login_required
 def createreview(request, ebook_id):
     ebook = get_object_or_404(Ebook, pk=ebook_id)
     if request.method == "GET":
         return render(
-            request, "createreview.html", {"form": ReviewForm(), "ebook": ebook}
+            request,
+            "createreview.html",
+            {
+                "form": ReviewForm(),
+                "ebook": ebook,
+            },
         )
     else:
         try:
@@ -50,3 +58,29 @@ def createreview(request, ebook_id):
                 "createreview.html",
                 {"form": ReviewForm(), "error": "bad data passed in"},
             )
+
+
+@login_required
+def updatereview(request, review_id):
+    review = get_object_or_404(Review, pk=review_id, user=request.user)
+    if request.method == "GET":
+        form = ReviewForm(instance=review)
+        return render(request, "updatereview.html", {"review": review, "form": form})
+    else:
+        try:
+            form = ReviewForm(request.POST, instance=review)
+            form.save()
+            return redirect("detail", review.ebook.id)
+        except ValueError:
+            return render(
+                request,
+                "updatereview.html",
+                {"review": review, "form": form, "error": "Bad data in form"},
+            )
+
+
+@login_required
+def deletereview(request, review_id):
+    review = get_object_or_404(Review, pk=review_id, user=request.user)
+    review.delete()
+    return redirect("detail", review.ebook.id)
